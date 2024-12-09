07f6b924ff9f65c5ebd4378a590adbf071cdfa3d
Nikita Luzianin <s2400796@edu.bc.fi>
2024-12-09 14:49:05 +0200

    add search

--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -11,6 +11,7 @@ import {
   Rating,
   Chip,
   Typography,
+  TextField
 } from '@mui/material';
 import useAxios from '../services/useAxios';
 
@@ -19,14 +20,15 @@ function Books() {
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     if (books.length === 0) {
       getBooks();
     }
   }, [data]);
+  // checks if books are fetched; if not - gets them form server
 
-  // TODO: Replace axios with useAxios hook
   async function getBooks() {
     try {
       await get('books');
@@ -36,19 +38,12 @@ function Books() {
       console.error(error);
     }
   }
-
-
-  // const books = useAxios().data;
-
-
-
-
-  // checks if books are fetched; if not - gets them form server
-
-  // TODO: Replace axios with useAxios hook
-
-
   // fetches books
+
+  const searchUpdate = (e) => {
+    setSearch(e.target.value.toLowerCase());
+
+  }
 
   // TODO: Implement search functionality
   return (
@@ -56,7 +51,17 @@ function Books() {
       {!books && <CircularProgress />}
       {/* if hte books are loading show loadign*/}
       {books && (
+
         <div>
+          <div className='search'>
+            <TextField sx={{ mb: 4 }}
+              name="search"
+              id="outlined-basic"
+              label="Search"
+              variant="outlined"
+              onChange={searchUpdate}
+            />
+          </div>
           <Stack
             sx={{ justifyContent: 'space-around' }}
             spacing={{ xs: 1 }}
@@ -66,59 +71,60 @@ function Books() {
           >
             {/* creates stacks of spaces for books*/}
             {books.map((book) => (
-              <Card
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  width: '15%',
-                  minWidth: 200,
-                }}
-                key={book.name}
+              (!search || book.name.toLowerCase().includes(search) || book.author.toLowerCase().includes(search)) && (
+                <Card
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: '15%',
+                    minWidth: 200,
+                  }}
+                  key={book.name}
 
-              >
-                {/* creates book card*/}
-                <CardMedia
-                  sx={{ height: 250 }}
-                  image={book.img}
-                  title={book.name}
-                />
-                <Box sx={{ pt: 2, pl: 2 }}>
-                  {book.genres.map((genre, i) => (
-                    <Chip
-                      key={i}
-                      label={genre}
-                      variant="outlined"
+                >
+                  {/* creates book card*/}
+                  <CardMedia
+                    sx={{ height: 250 }}
+                    image={book.img}
+                    title={book.name}
+                  />
+                  <Box sx={{ pt: 2, pl: 2 }}>
+                    {book.genres.map((genre, i) => (
+                      <Chip
+                        key={i}
+                        label={genre}
+                        variant="outlined"
+                        size="small"
+                      />
+
+                    ))}
+                    {/* creates pills for genres*/}
+                    <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
+                      {book.name}
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                      {book.author}
+                    </Typography>
+                  </Box>
+                  {/* creates text with autoher and book name*/}
+                  <CardActions
+                    sx={{
+                      justifyContent: 'space-between',
+                      mt: 'auto',
+                      pl: 2,
+                    }}
+                  >
+                    <Rating
+                      name="read-only"
+                      value={+book.stars}
+                      readOnly
                       size="small"
                     />
-
-                  ))}
-                  {/* creates pills for genres*/}
-                  <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
-                    {book.name}
-                  </Typography>
-                  <Typography variant="subtitle1" gutterBottom>
-                    {book.author}
-                  </Typography>
-                </Box>
-                {/* creates text with autoher and book name*/}
-                <CardActions
-                  sx={{
-                    justifyContent: 'space-between',
-                    mt: 'auto',
-                    pl: 2,
-                  }}
-                >
-                  <Rating
-                    name="read-only"
-                    value={+book.stars}
-                    readOnly
-                    size="small"
-                  />
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-                {/* creates two active elements - rating and a button*/}
-              </Card>
-            ))}
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                  {/* creates two active elements - rating and a button*/}
+                </Card>
+              )))}
           </Stack>
         </div>
       )}
